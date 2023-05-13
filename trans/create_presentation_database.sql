74d8c23b89124e20e8e02afc024fbb7e9b983902
khoinguyen19k8@gmail.com <khoinguyen19k8@gmail.com>
2023-05-13 23:41:12 +0000

    Create f1_presentation DB

---
+++ trans/create_presentation_database.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+CREATE DATABASE IF NOT EXISTS f1_presentation
+LOCATION "/mnt/formula1dlkhoinguyen19k8/presentation/";
+
+-- COMMAND ----------
+
+DESC DATABASE f1_presentation
