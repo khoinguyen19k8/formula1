a282752ae8f038a46b68a43940ff9acfec30a8c0
khoinguyen19k8@gmail.com <khoinguyen19k8@gmail.com>
2023-05-13 23:16:31 +0000

    Script that create f1_processed DB

---
+++ ingestion/create_processed_database.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+CREATE DATABASE IF NOT EXISTS f1_processed
+LOCATION "/mnt/formula1dlkhoinguyen19k8/processed/";
+
+-- COMMAND ----------
+
+DESC DATABASE f1_processed;
