cb5e86ac1e09bbbae8e88b1abb6e769f956e4a9e
khoinguyen19k8@gmail.com <khoinguyen19k8@gmail.com>
2023-05-13 23:16:16 +0000

    Add script to ingest all files at the same time

---
+++ ingestion/ingest_all_files.py
@@ -0,0 +1,30 @@
+# Databricks notebook source
+# MAGIC %run ingest_circuits_csv
+
+# COMMAND ----------
+
+# MAGIC %run ingest_construction_json
+
+# COMMAND ----------
+
+# MAGIC %run ingest_drivers_json
+
+# COMMAND ----------
+
+# MAGIC %run ingest_lap_times_csv
+
+# COMMAND ----------
+
+# MAGIC %run ingest_pitstops_json
+
+# COMMAND ----------
+
+# MAGIC %run ingest_qualifying_json
+
+# COMMAND ----------
+
+# MAGIC %run ingest_races_csv
+
+# COMMAND ----------
+
+# MAGIC %run ingest_results_json
